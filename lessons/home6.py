c5da636835812829f84f5cb117ba5def70415dbf
aitmanbetsul514@gmail.com <aitmanbetsul514@gmail.com>
2022-11-25 16:37:54 -1200

    Update home6.py

--- lessons/home6.py
+++ lessons/home6.py
@@ -1,43 +1,34 @@
+
 class Hero:
-    head = 1
 
-    def __init__(self,name,skill,hp):
-        self.name=name
-        self.skill = skill
-        self.hp = hp
+    def __init__(self, power=0, fly=0, skill=0, magic=0):
 
-    @property
-    def hello(self):
-        print('my name is ' + self.name)
-
-    @property
-    def fly(self):
-        print(f'{self.name} is flying')
-
-    @property
-    def heal(self,plus):
-        print(self.hp + plus)
-
-    @property
-    def phrase(self):
-        print('i won')
-
-
-    @hello.setter
-    def hello(self,new_hello):
-        self.new_hello = new_hello
-
-    @fly.setter
-    def fly(self,new_fly):
-        self.new_fly = new_fly
-
-
-    @heal.setter
-    def heal(self,new_heal):
-        self.new_heal = new_heal
-
-    @phrase.setter
-    def phrase(self,new_phrase):
-        self.new_phrase = new_phrase
-
-
+        self.__power = power
+        self.__fly = fly
+        self.__skill = skill
+        self.__magic = magic
+    def setpwr(self, power):
+        self.__power = power
+    def getpwr(self):
+        return self.__power
+    def setfly(self, fly):
+        self.__fly = fly
+    def getfly(self):
+        return self.__fly
+    def setskill(self, skill):
+        self.__skill = skill
+    def getskill(self):
+        return self.__skill
+    def setmgc(self, magic):
+        self.__magic = magic
+    def getmgc(self):
+        return self.__magic
+hr = Hero()
+hr.setpwr(33)
+hr.setfly(1)
+hr.setskill(100)
+hr.setmgc(24)
+print(f'pw-{hr.getpwr()} \n'
+f'fly-{hr.getfly()} \n'
+f'skill-{hr.getskill()} \n'
+f'Магия-{hr.getmgc()}')
